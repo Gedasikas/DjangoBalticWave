680ca6f0272cd3fa86c63d63e923f48fa9184859
Gedasikas <gediminascirtautas@gmail.com>
2023-02-16 16:13:53 +0200

    Admin Site updated

--- mysite/balticwave/admin.py
+++ mysite/balticwave/admin.py
@@ -1,9 +1,40 @@
 from django.contrib import admin
-
 from .models import Sailor, Product, Service, ServiceType, ProductType
 
-admin.site.register(Sailor)
-admin.site.register(Product)
-admin.site.register(Service)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('product_name', 'seller', 'insDate')
+    list_filter = ('status', 'type')
+    search_fields = ('product_name', 'seller')
+
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('service_name', 'seller')
+    list_filter = ('type',)
+    search_fields = ('service_name', 'seller')
+
+
+class ProductInline(admin.TabularInline):
+    model = Product
+    readonly_fields = ('product_name', 'description', 'price', 'type', 'status',)
+    can_delete = False
+    extra = 0
+
+
+class ServiceInline(admin.TabularInline):
+    model = Service
+    readonly_fields = ('service_name', 'description', 'type')
+    can_delete = False
+    extra = 0
+
+
+class SailorAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'birth_day')
+    inlines = [(ProductInline), (ServiceInline)]
+
+
+admin.site.register(Sailor, SailorAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(ServiceType)
 admin.site.register(ProductType)
--- mysite/balticwave/models.py
+++ mysite/balticwave/models.py
@@ -3,6 +3,10 @@ from tinymce.models import HTMLField
 class Sailor(models.Model):
     nickname = models.CharField('Nickname', max_length=50)
     birth_day = models.DateField('Date of birth')
+
+    def __str__(self):
+        return self.nickname
+
 class Product(models.Model):
     product_name = models.CharField('Product name', max_length=200)
     seller = models.ForeignKey('Sailor', on_delete=models.CASCADE, blank=True)
@@ -23,17 +27,25 @@ class Product(models.Model):
         default='a',
         help_text='Status',
     )
+    def __str__(self):
+        return (f'{self.product_name} | {self.seller}')
 class Service(models.Model):
     service_name = models.CharField('Service name', max_length=200)
     seller = models.ForeignKey('Sailor', on_delete=models.CASCADE, blank=True)
     description = HTMLField()
-    type = models.ManyToManyField('ProductType')
+    type = models.ManyToManyField('ServiceType')
+    def __str__(self):
+        return (f'{self.service_name} | {self.seller}')
 
 class ServiceType(models.Model):
     service_type_name = models.CharField('Type', max_length=50)
+
+    def __str__(self):
+        return self.service_type_name
 class ProductType(models.Model):
     product_type_name = models.CharField('Type', max_length=50)
-
+    def __str__(self):
+        return self.product_type_name
 
 
 

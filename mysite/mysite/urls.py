8eddea10476fbf18a1a30a793c66916452d54dcf
Gedasikas <gediminascirtautas@gmail.com>
2023-02-17 20:47:03 +0200

    Added Home page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('balticwave/', include('balticwave.urls')),
     path('', RedirectView.as_view(url='balticwave/', permanent=True)),
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

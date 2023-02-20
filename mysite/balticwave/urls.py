8370e815449629b9e9948d75b6736f8164f650d3
Gedasikas <gediminascirtautas@gmail.com>
2023-02-20 10:07:32 +0200

    Added basic serive list + Product and Service DetailView

--- mysite/balticwave/urls.py
+++ mysite/balticwave/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('products/', views.ProductListView.as_view(), name='products')
+    path('products/', views.ProductListView.as_view(), name='products'),
+    path('products/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
+    path('services/', views.ServiceListView.as_view(), name='services'),
+    path('services/<int:pk>', views.ServiceDetailView.as_view(), name='service-detail'),
 ]
--- mysite/balticwave/views.py
+++ mysite/balticwave/views.py
@@ -10,3 +10,16 @@ class ProductListView(generic.ListView):
     template_name = 'products.html'
     context_object_name = 'products'
 
+class ProductDetailView(generic.DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+class ServiceListView(generic.ListView):
+    model = Service
+    template_name = 'services.html'
+    context_object_name = 'services'
+
+class ServiceDetailView(generic.DetailView):
+    model = Service
+    template_name = 'service_detail.html'
+    context_object_name = 'service'

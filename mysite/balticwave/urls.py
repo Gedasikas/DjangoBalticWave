9284eb38a6b94cc18ff3cd9275ffd9bf1dc2b577
Gedasikas <gediminascirtautas@gmail.com>
2023-02-20 09:41:11 +0200

    Added basic product list

--- mysite/balticwave/urls.py
+++ mysite/balticwave/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('products/', views.ProductListView.as_view(), name='products')
 ]
--- mysite/balticwave/views.py
+++ mysite/balticwave/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+from .models import Product, Service
 def home(request):
     return render(request, 'home.html')
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products.html'
+    context_object_name = 'products'
+
